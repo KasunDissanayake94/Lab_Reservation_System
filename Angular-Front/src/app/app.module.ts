5768b4fc642cf3899a51ebb32bcb4892a6c686c3
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2018-06-04 22:50:09 +0530

    Login Added

--- Angular-Front/src/app/app.module.ts
+++ Angular-Front/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Router, Routes} from '@angular/router'
 
 
 import { AppComponent } from './app.component';
+import { SlidebarComponent } from './slidebar/slidebar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+const applicationRouters:Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(applicationRouters),
   ],
   providers: [],
   bootstrap: [AppComponent]
