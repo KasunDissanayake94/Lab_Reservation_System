548ea09da44396177bfa4dca5b05c3f2ceaf79dc
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2018-06-14 05:33:33 +0530

    All labs inserted

--- Angular-Front/src/app/vreservations/vreservations.component.ts
+++ Angular-Front/src/app/vreservations/vreservations.component.ts
@@ -15,6 +15,8 @@ export class VreservationsComponent implements OnInit {
 
   model: NgbDateStruct;
   date: {year: number, month: number};
+  //defining week days
+  public labs = ['LAB A','LAB B','LAB C','LAB D','LAB E','ELECTRONIC LAB','3RD YEAR LAB','4TH YEAR LAB','x'];
 
   selectToday() {
     this.model = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
@@ -23,5 +25,9 @@ constructor(){
 
 }
   ngOnInit() {
+  }
+
+  all_reservations() {
+    console.log("method called");
   }
 }
